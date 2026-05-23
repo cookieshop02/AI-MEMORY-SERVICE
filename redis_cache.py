1d4fa5f25c6d8ce1943263ca98bc743ac11792fc
cookieshop02 <work.cookie.0212@gmail.com>
2026-05-23 18:42:28 +0530

    added redis

---
+++ redis_cache.py
@@ -0,0 +1,20 @@
+import redis
+import json
+
+r = redis.Redis(host ="localhost", port = 6380, decode_responses = True)
+
+#store cache
+def create_cache(user_id, messages):
+    
+    serialized = []
+    for msg in serialized:
+        serialized.append({
+            "message": msg.message,
+            "sentiment": msg.sentiment
+        })
+
+    r.set(
+        f"user:{user_id}",
+        json.dumps(serialized)
+        ex = 60
+    )
